54fbad523e115005d1f9039aa5e86f3366d0bec9
Anna Madyakina <annachuvatkina1906@gmail.com>
2022-06-22 20:28:19 +0500

    allWordTable

--- client/src/components/AllWord/OneWord/OneWord.jsx
+++ client/src/components/AllWord/OneWord/OneWord.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import styles from './OneWord.module.css'
 
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+
 
 export default function OneWord({ word }) {
   return (
     <>
-<div className='container'>
-<img src={word.img} class="d-block w-10" alt="..." />
-<h2>{word.wordEnglish}, {word.wordRussian}</h2>
-</div>
+             <TableRow
+              key={word.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row" align="center">{word.wordEnglish}
+              </TableCell>
+              <TableCell align="center">{word.wordRussian}</TableCell>
+            </TableRow>    
 </>
   )
 }
--- server/routes/letterRout.js
+++ server/routes/letterRout.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const { Topic, TopicImg, Word, InsertLetter } = require('../db/models')
 
 router.get('/all', async (req, res) => {
-
-  const allWord = await InsertLetter.findAll({include: {model: Topic, model: Word}, raw: true})
-  res.json(allWord)
+try {
+const allWord = await InsertLetter.findAll({include: {model: Topic, model: Word}, raw: true})
+  res.json(allWord)  
+} catch (error) {
+  console.log(error)
+}
+  
   })
 
   
