3fa32f06c87f2b99681145ea529026555ed2b35b
Taras Gavrilyuk <tarastim@gmail.com>
2022-06-23 13:22:59 +0300

    error fixed, refactoring words

--- client/src/components/WordsPage/OneWords/OneWords.jsx
+++ client/src/components/WordsPage/OneWords/OneWords.jsx
@@ -15,7 +15,6 @@ export default function OneWords() {
 
   const [count, setCount] = useState(0)
   const [words, setWords] = useState([])
-  const [option, setOption] = useState([])
   const [checkAnswer, setCheckAnswer] = useState(0)
   const [trueAnswers, setTrueAnswers] = useState(0)
 
@@ -24,7 +23,6 @@ export default function OneWords() {
       .then((data) => {
         const words = data.data.filter((el) => el.topicId === Number(id))
         setWords(words)
-        setOption(words[0].option.split(''))
       })
   }, [])
 
@@ -33,14 +31,13 @@ export default function OneWords() {
     // console.log(event.target.value)
     // console.log(words[count])
     // console.log(words[0])
-    // console.log(words[count].text)
+    // console.log(words[count].text.split('').map(el => el.toUpperCase()).join(''))
     // console.log(words[count].letter)
     // console.log(words[count]['Word.img'])
     if (event.target.value === words[count]?.letter) {
       console.log('правильный ответ')
       setCheckAnswer(0)
       setCount(count + 1)
-      setOption(words[count + 1]?.option.split(''))
       setTrueAnswers(trueAnswers + 1)
     } else if (checkAnswer < 1) {
       setCheckAnswer(checkAnswer + 1)
@@ -49,27 +46,23 @@ export default function OneWords() {
       console.log('провалил все попытки')
       setCheckAnswer(0)
       setCount(count + 1)
-      setOption(words[count + 1]?.option.split(''))
     }
   }
-
-  {/* <h3>Молодец, ты верно заполнил {trueAnswers} слова</h3> */ }
-  // {count === words.length? <h3>Молодец, правильных ответов: {trueAnswers}</h3> : null}
 
   return (
     <>
       {words[count] ? 
-      <>
+      (<>
       <img className={styles.Img} src={words[count]['Word.img']} alt="" />
-      <div className={styles.Word}>{words[count].text}</div>
+      <div className={styles.Word}>{words[count].text.split('').map(el => el.toUpperCase()).join('')}</div>
       {checkAnswer ? <div>попробуй еще разок</div> : null}
        <ButtonGroup className={styles.Btn} variant="outlined" size="small" aria-label="outlined button group">
-        <Button onClick={pushHandler} value={option[0]}>{option[0]}</Button>
-        <Button onClick={pushHandler} value={option[1]}>{option[1]}</Button>
-        <Button onClick={pushHandler} value={option[2]}>{option[2]}</Button>
+        {words[count]?.option.split('').map((el, i) => {
+        return <Button onClick={pushHandler} value={el} key={i}>{el}</Button>
+        })}
       </ButtonGroup>
-      </>
-       : <h3>Молодец, правильных ответов: {trueAnswers}</h3>}
+      </>)
+       : (count ? <h3>Молодец, правильных ответов: {trueAnswers}</h3> : null)}
     </>
   )
 }
