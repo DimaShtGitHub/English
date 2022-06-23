34767c848f17389b4594d12a1f82ba1b9eda1760
Anna Madyakina <annachuvatkina1906@gmail.com>
2022-06-23 11:13:33 +0500

    word

--- client/src/components/AllWord/OneWord/OneWord.jsx
+++ client/src/components/AllWord/OneWord/OneWord.jsx
@@ -6,12 +6,20 @@ import TableRow from '@mui/material/TableRow';
 
 
 export default function OneWord({ word }) {
+  const talk = () => {
+    var synth = window.speechSynthesis;
+    var utterThis = new SpeechSynthesisUtterance(word.wordEnglish);
+     synth.speak(utterThis);
+}
+
+
   return (
     <>
              <TableRow
               key={word.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row" align="center">{word.wordEnglish}
+              <TableCell   component="th" scope="row" align="center">
+               <button onClick={talk}> {word.wordEnglish}</button>
               </TableCell>
               <TableCell align="center">{word.wordRussian}</TableCell>
             </TableRow>    
