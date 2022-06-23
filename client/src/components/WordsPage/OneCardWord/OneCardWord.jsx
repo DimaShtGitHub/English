b72e46710062974ec5c35462294a3c9a4d5cfcb2
Anna Madyakina <annachuvatkina1906@gmail.com>
2022-06-23 13:25:24 +0500

    cardWord

---
+++ client/src/components/WordsPage/OneCardWord/OneCardWord.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from './OneCardWord.module.css'
+import { useNavigate } from 'react-router-dom' 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function OneCardWord({ topic }) {
+
+
+  const navigate = useNavigate(); 
+  return (
+    <>
+    <Card sx={{ minWidth: '250px', maxWidth: '250px' }} onClick={()=> navigate(`/words/${topic.id}`, {replace: true})}>
+    <CardActionArea >
+      <CardMedia
+        component="img"
+        variant="quilted"
+         image={topic['TopicImg.img']}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {topic.title}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    </Card>
+</>
+  )
+}
