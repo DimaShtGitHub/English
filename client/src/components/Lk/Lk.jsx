d30ba3335deb6ff491bbc5c765454c347cb798dc
Anna Madyakina <annachuvatkina1906@gmail.com>
2022-06-24 23:20:13 +0500

    lk style

--- client/src/components/Lk/Lk.jsx
+++ client/src/components/Lk/Lk.jsx
@@ -3,10 +3,12 @@ import styles from './Lk.module.css'
 import axios from 'axios'
 import Statistic from './Statistic/Statistic'
 import { useEffect, useState } from 'react'
+import {  useNavigate } from 'react-router-dom';
+
 
 export default function Lk() {
 const [allStatistic, setAllStatistic] = useState([])
-
+const navigate = useNavigate();
 useEffect(()=> {
   axios.get('http://localhost:3001/statistic/user', {withCredentials: true})
   .then(data => setAllStatistic(data.data))
@@ -14,11 +16,22 @@ useEffect(()=> {
 
   return (
     <>
-    <div><h2>Слова, которые нужно повторить</h2>
+    <div className={styles.Home}>
+        {allStatistic.length > 0 ? (
+          <>
+<h3>Слова, которые нужно повторить</h3>
     <div className={styles.container}> 
-    {allStatistic?.length > 0 ? (
-      allStatistic.map(el => <Statistic key={el.id} word={el} /> )):(null)}
-      </div>
+     {allStatistic.map(el => <Statistic key={el.id} word={el} /> )} 
+      </div></>
+        ):(
+          <>
+          <h3 className={styles.hh3}>У тебя нет слов, которые нужно повторить!</h3>
+          <h4  onClick={() => {navigate("/", { replace: true })}}>Пошли играть!</h4>
+          <img src="/img/lk.png" alt="lk.png" className={styles.img} onClick={() => {navigate("/", { replace: true })}}/>
+          </>
+        )}
+
+    
       </div>
     </>
   )
