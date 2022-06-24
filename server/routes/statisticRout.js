032bb32e03e4feff6ec84e5135bf3a5e087eb5d0
Anna Madyakina <annachuvatkina1906@gmail.com>
2022-06-24 12:02:21 +0500

    02

--- server/routes/statisticRout.js
+++ server/routes/statisticRout.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {  Word, Statistic } = require('../db/models')
 
+router.get('/user', async(req, res) => {
+  const id = req.session?.user?.id
+  const usersStatistic = await Statistic.findAll({where: {userId: id},include: {model: Word}, raw: true})
+  res.json(usersStatistic)
+})
+
+
 router.post('/', async (req, res) => {
   try {
   const truearr = req.body.stat.arrtrue
