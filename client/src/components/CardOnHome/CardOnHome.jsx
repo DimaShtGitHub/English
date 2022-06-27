eae50c13e5b7dfef2864ec4e9ddeb10588d2470f
Dima <schtanckevitch.dima@yandex.ru>
2022-06-27 17:28:19 +0300

    upgrate header: add new game and sound btn up

--- client/src/components/CardOnHome/CardOnHome.jsx
+++ client/src/components/CardOnHome/CardOnHome.jsx
@@ -24,100 +24,98 @@ export default function CardOnHome() {
   const navigate = useNavigate(); 
   return (
     <>
-    <Container  className={styles.Container}>
-    <Card  className={styles.Card} >
-      <CardActionArea className={styles.Area} onClick={()=> navigate('/coloring', {replace: true})}>
-        <img className={styles.Img}
-          src="/img/Coloring.png"
-          alt='Coloring.png'
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Coloring
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Тут можно раскрасить картинку
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Container  className={styles.Container}>
+        <Card  className={styles.Card} >
+          <CardActionArea className={styles.Area} onClick={()=> navigate('/coloring', {replace: true})}>
+            <img className={styles.Img}
+              src="/img/Coloring.png"
+              alt='Coloring.png'
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Coloring
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Тут можно раскрасить картинку
+              </Typography>
+            </CardContent>
+          </CardActionArea>
           <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk('Тут можно раскрасить картинку') : alert('включите звук, кнопочка в меню')}/>
-      </Card>
+        </Card>
 
-      <Card  className={styles.Card} >
-      <CardActionArea className={styles.Area} onClick={()=> navigate('/card', {replace: true})}>
-        <img className={styles.Img}
-            src="/img/Card.png"
-            alt='Coloring.png'
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Card
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           В этой игре нужно будет по картинке выбрать правильное слово
-          </Typography>
-        </CardContent>
-      </CardActionArea >
-      <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk('В этой игре нужно будет по картинке выбрать правильное слово') : null}/>
-    </Card>
+        <Card  className={styles.Card} >
+          <CardActionArea className={styles.Area} onClick={()=> navigate('/card', {replace: true})}>
+            <img className={styles.Img}
+                src="/img/Card.png"
+                alt='Coloring.png'
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Card
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              В этой игре нужно будет по картинке выбрать правильное слово
+              </Typography>
+            </CardContent>
+          </CardActionArea >
+          <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk('В этой игре нужно будет по картинке выбрать правильное слово') : null}/>
+        </Card>
 
-      <Card className={styles.Card} >
-      <CardActionArea className={styles.Area} onClick={()=> navigate('/words', {replace: true})}>
-        <img className={styles.Img}
-          src="/img/Word.png"
-          alt='Word.png'
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Words
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           В этой игре нужно будет подставлять буквы в слова с иллюстрациями
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk('В этой игре нужно будет подставлять буквы в слова с иллюстрациями') : null}/>
-    </Card>
+        <Card className={styles.Card} >
+          <CardActionArea className={styles.Area} onClick={()=> navigate('/words', {replace: true})}>
+            <img className={styles.Img}
+              src="/img/Word.png"
+              alt='Word.png'
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Words
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              В этой игре нужно будет подставлять буквы в слова с иллюстрациями
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk('В этой игре нужно будет подставлять буквы в слова с иллюстрациями') : null}/>
+        </Card>
 
-<Card  className={styles.Card} >
-      <CardActionArea className={styles.Area} onClick={()=> navigate('/dictionary', {replace: true})}>
-        <img className={styles.Img}
-         src="/img/Dictionary.png"
-         alt='Dictionary.png'
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-          Dictionary
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           Это словарик. Тут можно посмотреть все слова
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk(' Это словарик. Тут можно посмотреть все слова') : null}/>
-      </Card>
-
-
-
-
-      <Card  className={styles.Card} >
-      <CardActionArea className={styles.Area} onClick={()=> navigate('/sound', {replace: true})}>
-        <img className={styles.Img}
-          src="/img/SoundGame.png"
-          alt='SoundGame.png'
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-          SoundGame
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            По звуку нажми на правильную картинку
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+        <Card  className={styles.Card} >
+          <CardActionArea className={styles.Area} onClick={()=> navigate('/sound', {replace: true})}>
+            <img className={styles.Img}
+              src="/img/SoundGame.png"
+              alt='SoundGame.png'
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+              SoundGame
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                По звуку нажми на правильную картинку
+              </Typography>
+            </CardContent>
+          </CardActionArea>
           <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk('По звуку нажми на правильную картинку') : alert('включите звук, кнопочка в меню')}/>
-      </Card>
-    </Container>
+        </Card>
+
+        <Card  className={styles.Card} >
+          <CardActionArea className={styles.Area} onClick={()=> navigate('/dictionary', {replace: true})}>
+            <img className={styles.Img}
+            src="/img/Dictionary.png"
+            alt='Dictionary.png'
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+              Dictionary
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              Это словарик. Тут можно посмотреть все слова
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <VolumeUpIcon className={styles.Volume} onClick={()=> sound ? talk(' Это словарик. Тут можно посмотреть все слова') : null}/>
+        </Card>
+                        
+      </Container>
     </>
   )
 }
